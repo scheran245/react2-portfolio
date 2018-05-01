ebd47b807224e1c0b0c06d2263779a3595b11a44
senguttuvan s <scheran245@gmail.com>
2018-05-01 23:24:39 +0800

    setup prod:build server

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,35 @@
-module.exports = {
-    entry:'./src/app.js',
-    output:{
-        path:__dirname+'/public/',
-        filename:'app.js'
-    },
-    module:{
-        rules:[{
-            loader:'babel-loader',
-            test:/\.js$/,
-            exclude:/node_modules/
-        },{
-            test:/\.s?css$/,
-            use:[
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    devtool:'cheap-module-eval-source-map',
-    devServer:{
-        contentBase:__dirname+'/public/',
-        historyApiFallback:true
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = (env) =>{
+    // console.log('env = '+env);
+    const isProduction = env === 'production';
+
+    return {
+        entry:'./src/app.js',
+        output:{
+            path:__dirname+'/public/',
+            filename:'app.js'
+        },
+        module:{
+            rules:[{
+                loader:'babel-loader',
+                test:/\.js$/,
+                exclude:/node_modules/
+            },{
+                test:/\.s?css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                  })
+            }]
+        },
+        plugins: [
+          new ExtractTextPlugin("styles.css")
+        ],
+        devtool:isProduction ? 'source-map':'cheap-module-eval-source-map',
+        devServer:{
+            contentBase:__dirname+'/public/',
+            historyApiFallback:true
+        }
     }
 }
